70ca8bc04fd7aba96e260ebdda2a7118ea395dc4
ivankishko <mistedphone@gmail.com>
2021-06-03 10:40:03 +0300

    minor cosmetic changes

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -12,13 +12,12 @@ function Header() {
                 <button className={classes.navButton} onClick={changeMode}>{visible ? `Menu <<` : `Menu >>`}</button>
             </div>
             <div className={visible ? classes.navigationAppear : classes.navigationHide}>
-                <NavLink onClick={changeMode} className={classes.link} to="/pre-junior">pre-junior level homeworks</NavLink>
+                <NavLink onClick={changeMode} className={classes.link} to="/pre-junior">pre-junior level
+                    homeworks</NavLink>
                 <NavLink onClick={changeMode} className={classes.link} to="/junior">junior level homeworks</NavLink>
-                <NavLink onClick={changeMode} className={classes.link} to="/junior-plus">junior-plus level homeworks</NavLink>
+                <NavLink onClick={changeMode} className={classes.link} to="/junior-plus">junior-plus level
+                    homeworks</NavLink>
             </div>
-            {/*<div>*/}
-            {/*    <button className={`${classes.navButton} ${visible ? classes.navButtonAppear : classes.navButtonHide}`} onClick={changeMode}>{visible ? `<< nav ` : `nav >>`}</button>*/}
-            {/*</div>*/}
         </div>
     )
 }
