86ae28d49eabc538b197559128c93f71c47e416b
ivankishko <mistedphone@gmail.com>
2021-06-15 00:50:21 +0300

    h7 - select and radio (will add styles later)

---
+++ src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
@@ -0,0 +1,37 @@
+import React, {SelectHTMLAttributes, DetailedHTMLProps, ChangeEvent} from 'react'
+
+type DefaultSelectPropsType = DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>
+
+type SuperSelectPropsType = DefaultSelectPropsType & {
+    options?: any[]
+    onChangeOption?: (option: any) => void
+}
+
+const SuperSelect: React.FC<SuperSelectPropsType> = (
+    {
+        options,
+        onChange, onChangeOption,
+        ...restProps
+    }
+) => {
+    // map options with key
+    const mappedOptions: any[] = options ? options.map((o, i) => {
+        return (
+            <option key={o.value + i} value={o}>{o}</option>
+        )
+    }) : [];
+
+    const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
+        // onChange, onChangeOption
+        onChange && onChange(e)
+        onChangeOption && onChangeOption(e.currentTarget.value)
+    }
+
+    return (
+        <select onChange={onChangeCallback} {...restProps}>
+            {mappedOptions}
+        </select>
+    )
+}
+
+export default SuperSelect
