6c21df565cea340fd1fcdddd97321bfabb63b6c0
ivankishko <mrivankishko@gmail.com>
2021-06-28 15:08:06 +0300

    hw9 reducers

---
+++ src/p2-homeworks/h9/Clock.tsx
@@ -0,0 +1,57 @@
+import React, {useState} from 'react'
+import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+
+function Clock() {
+    const [timerId, setTimerId] = useState<number>(0)
+    const [date, setDate] = useState<Date>(new Date())
+    const [show, setShow] = useState<boolean>(false)
+
+    const stop = () => {
+        // stop
+        window.clearInterval(timerId)
+    }
+    const start = () => {
+        stop()
+        const id: number = window.setInterval(() => {
+            // setDate
+            setDate(new Date())
+            return date.getTime()
+        }, 1000)
+        setTimerId(id)
+    }
+
+    const onMouseEnter = () => {
+        // show
+        setShow(true)
+    }
+    const onMouseLeave = () => {
+        // close
+        setShow(false)
+    }
+
+    const stringTime = date.toLocaleTimeString() // fix with date
+    const stringDate = date.toLocaleDateString() // fix with date
+
+    return (
+        <div>
+            <div
+                onMouseEnter={onMouseEnter}
+                onMouseLeave={onMouseLeave}
+                style={{marginLeft: '5px'}}
+            >
+                {stringTime}
+            </div>
+
+            {show && (
+                <div style={{marginLeft: '5px'}}>
+                    {stringDate}
+                </div>
+            )}
+
+            <SuperButton onClick={start}>start</SuperButton>
+            <SuperButton onClick={stop}>stop</SuperButton>
+        </div>
+    )
+}
+
+export default Clock
